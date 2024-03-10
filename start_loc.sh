c71e80d2656a5477e24ef2178e28a0e012a1afb1
Semyon <semhowork@gmail.com>
2024-03-10 10:00:04 +0300

    updated docker-compose

--- start_loc.sh
+++ start_loc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker compose --env-file .env.local -f loc.docker-compose.yaml up --build
+
+#docker build -t <img> -f <name>.Dockerfile .
 #go run <img> -config-path=.env.local
 # <img> - название или id образа, -config-path=.env.local  - имя файла с переменными окружения
--- start_prod.sh
+++ start_prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker compose --env-file .env.prod -f prod.docker-compose.yaml up --build
+
+#docker build -t <img> -f <name>.Dockerfile .
 #go run <img> -config-path=.env.local
 # <img> - название или id образа, -config-path=.env.local  - имя файла с переменными окружения
